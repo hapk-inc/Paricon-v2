961bd2814dc1f4a8991307ebed2af3fbc75edb91
Harish <harish@clevertap.com>
2023-10-02 00:44:11 +0530

    Oct 2 12:45 am

---
+++ lib/router/my_route.gr.dart
@@ -0,0 +1,179 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'my_route.dart';
+
+abstract class _$MyRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$MyRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    AppStackRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AppStackPage(),
+      );
+    },
+    AppUpdateRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AppUpdatePage(),
+      );
+    },
+    DashboardRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const DashboardPage(),
+      );
+    },
+    ErrorRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const ErrorPage(),
+      );
+    },
+    LoginRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const LoginPage(),
+      );
+    },
+    MaintenanceRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const MaintenancePage(),
+      );
+    },
+    SplashRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SplashPage(),
+      );
+    },
+    TournamentRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const TournamentPage(),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [AppStackPage]
+class AppStackRoute extends PageRouteInfo<void> {
+  const AppStackRoute({List<PageRouteInfo>? children})
+      : super(
+          AppStackRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AppStackRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [AppUpdatePage]
+class AppUpdateRoute extends PageRouteInfo<void> {
+  const AppUpdateRoute({List<PageRouteInfo>? children})
+      : super(
+          AppUpdateRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AppUpdateRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [DashboardPage]
+class DashboardRoute extends PageRouteInfo<void> {
+  const DashboardRoute({List<PageRouteInfo>? children})
+      : super(
+          DashboardRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'DashboardRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [ErrorPage]
+class ErrorRoute extends PageRouteInfo<void> {
+  const ErrorRoute({List<PageRouteInfo>? children})
+      : super(
+          ErrorRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ErrorRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [LoginPage]
+class LoginRoute extends PageRouteInfo<void> {
+  const LoginRoute({List<PageRouteInfo>? children})
+      : super(
+          LoginRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'LoginRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [MaintenancePage]
+class MaintenanceRoute extends PageRouteInfo<void> {
+  const MaintenanceRoute({List<PageRouteInfo>? children})
+      : super(
+          MaintenanceRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'MaintenanceRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SplashPage]
+class SplashRoute extends PageRouteInfo<void> {
+  const SplashRoute({List<PageRouteInfo>? children})
+      : super(
+          SplashRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SplashRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [TournamentPage]
+class TournamentRoute extends PageRouteInfo<void> {
+  const TournamentRoute({List<PageRouteInfo>? children})
+      : super(
+          TournamentRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'TournamentRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
